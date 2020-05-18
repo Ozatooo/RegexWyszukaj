7d0e2e1c7b9a084a2a9e78a77853f2cdf883991f
Ozatooo <61974211+Ozatooo@users.noreply.github.com>
2020-05-18 09:19:00 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <string>
+#include <math.h>
+#include <regex>
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+ 
+using namespace std;
+ 
+int main(int argc, char** argv) {
+ 
+string wyszukaj;
+cout<<"wpisz co chcesz wyszukac : ";
+cin>>wyszukaj;
+string input;
+ regex regularne(wyszukaj); //tworzymy zmienn¹ z wyra¿eniem
+    ofstream plik1;
+   ifstream plik;  //deklaracja zmiennej pliku tekstowego
+   
+   plik.open("a.txt");  //otwarcie pliku
+   
+   if(plik.good())  //sprawdzenie czy plik istnieje
+            while(!plik.eof())  //petla wykonuje sie az program dojedzie do konca pliku
+        {
+           
+              plik>>input;
+              cout<<endl;
+             if(!cin) break;
+                if(input=="koniec") //koniec pêtli
+                        break;
+                if(regex_match(input,regularne)) //sprawdzamy, czy wprowadzony napis pasuje do wyra¿enia
+                {
+                	cout<<input;
+                        
+                        
+                    }
+
+               
+                     
+        }
+               
+             
+                   plik.close();  //zamkniecie pliku tekstowego
+ 
+cout<<endl;
+ 
+       
+ return 0;
+}
+
